45d647e2eb8dd92fc4e22f2c9ae6ad652dec23cb
Parth Patel <>
2022-11-20 16:30:46 -0500

    add another tab
    discover tab

---
+++ lib/MyNotes.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/screens/start_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+class MyNotes extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context){
+        return MaterialApp(
+      title: 'NoteKeeper',
+      debugShowCheckedModeBanner: true,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        textTheme: TextTheme(
+          headline5: TextStyle(
+              fontFamily: 'Sans',
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+              fontSize: 24),
+          bodyText2: TextStyle(
+              fontFamily: 'Sans',
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+              fontSize: 20),
+          bodyText1: TextStyle(
+              fontFamily: 'Sans',
+              fontWeight: FontWeight.normal,
+              color: Colors.black,
+              fontSize: 18),
+          subtitle2: TextStyle(
+              fontFamily: 'Sans',
+              fontWeight: FontWeight.normal,
+              color: Colors.black,
+              fontSize: 14),
+        ),
+      ),
+      home: startpage(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,47 +2,64 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/screens/start_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import './MyNotes.dart' as myNotes;
+import './Discover.dart' as discover;
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(MyApp());
+  runApp(new MaterialApp(
+    home: new MyTabs()
+  ));
 }
 
-class MyApp extends StatelessWidget {
+class MyTabs extends StatefulWidget{
+  @override
+  MyApp createState() => new MyApp();
+}
+
+
+class MyApp extends State<MyTabs> with SingleTickerProviderStateMixin {
+
+  TabController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = new TabController(vsync: this, length: 2);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'NoteKeeper',
-      debugShowCheckedModeBanner: true,
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        textTheme: TextTheme(
-          headline5: TextStyle(
-              fontFamily: 'Sans',
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-              fontSize: 24),
-          bodyText2: TextStyle(
-              fontFamily: 'Sans',
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-              fontSize: 20),
-          bodyText1: TextStyle(
-              fontFamily: 'Sans',
-              fontWeight: FontWeight.normal,
-              color: Colors.black,
-              fontSize: 18),
-          subtitle2: TextStyle(
-              fontFamily: 'Sans',
-              fontWeight: FontWeight.normal,
-              color: Colors.black,
-              fontSize: 14),
-        ),
+
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("Memoir"), backgroundColor: Colors.deepPurple
+    ),
+    bottomNavigationBar: new Material(
+      color: Colors.deepPurple,
+      child: new TabBar(
+        controller: controller,
+        tabs: <Tab>[
+            new Tab(icon: new Icon(Icons.account_circle)),
+            new Tab(icon: new Icon(Icons.public))
+          ]
+        )
       ),
-      home: startpage(),
+      body: new TabBarView(
+        controller: controller,
+        children: <Widget>[
+          new myNotes.MyNotes(),
+          new discover.Discover()
+        ]
+      )
     );
   }
 }
